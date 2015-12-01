56a7c8e83501197849f64d675dc82f9b50936043
JamesJim <james.clodfelder@gmail.com>
2015-12-01 15:46:59 -0800

    Added Drop Down Filters

--- js/app.js
+++ js/app.js
@@ -40,10 +40,64 @@
     }
     sortByDate(blog.rawData);
 
-    //temporarily store data in array to create new article then print to DOM
+    //temporarily store data in array to create new instance for each article then print to DOM
     var dataArray = {};
     for(i=0; i<blog.rawData.length; i++){
       dataArray = new makeArticle(blog.rawData[i]);
       $('#articles').append(dataArray.toHtml());
     }
+  });
+
+  //Filter arrays and return unique values
+  $(function() {
+
+    //function to build array of all categories from blog.rawData
+    function getCategoryFilterItems(){
+      var tempFilterArray = [];
+      for(i=0; i<blog.rawData.length; i++){
+        tempFilterArray.push(blog.rawData[i].category);
+      }
+      console.log(tempFilterArray);
+      return tempFilterArray;
+    }
+    var categoryStrings = getCategoryFilterItems();
+
+    //function to build array of all authors from blog.rawData
+    function getAuthorFilterItems(){
+      var tempFilterArray = [];
+      for(i=0; i<blog.rawData.length; i++){
+        tempFilterArray.push(blog.rawData[i].author);
+      }
+      console.log(tempFilterArray);
+      return tempFilterArray;
+    }
+    var authorStrings = getAuthorFilterItems();
+
+    //Function to put all the unique items in an array
+    function getUnique(inputArray){
+
+      var outputArray = [];
+
+      for (i=0; i < inputArray.length; i++){
+        if (($.inArray(inputArray[i], outputArray)) == -1)
+        {
+          outputArray.push(inputArray[i]);
+        }
+      }
+      return outputArray;
+    }
+    var uniqueCategories = getUnique(categoryStrings);
+    var uniqueAuthors = getUnique(authorStrings);
+
+    //Functions to print unique arrays and option tags to the select tag
+    function printToSelect(array, elementId){
+      for(i=0; i<array.length; i++){
+        $(elementId).append("<option value='"+array[i]+"'>"+array[i]+"</option>");
+        console.log(array[i]);
+      }
+    }
+    printToSelect(uniqueCategories, '#category-filter');
+    printToSelect(uniqueAuthors, '#author-filter');
+
+
   });
